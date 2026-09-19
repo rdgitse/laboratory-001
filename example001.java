5a44fe3fa38d519851e41280f6b67c7fe26adc98
rdgitse <ricardo.degracia9385@itse.ac.pa>
2026-09-18 19:57:06 -0500

    Cree mi primer Hola Mundo y cree mi primera calculadora

---
+++ example001.java
@@ -0,0 +1,9 @@
+public class example001 {
+
+public static void main (String[]args){
+
+    System.out.println("Hola Mundo!");
+    
+}
+
+}
---
+++ example002.java
@@ -0,0 +1,13 @@
+public class example002 {
+    
+    public static void main (String[]args){
+
+        int number1=50;
+        int number2=40;
+        int sum=0;
+
+        sum=number1/number2;
+        System.out.println("La división es: " + sum);
+    }
+
+}
